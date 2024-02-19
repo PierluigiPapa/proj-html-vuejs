fb8eff21c8a950ee2c729ba059d61edb5295a96a
PierluigiPapa <pierluigipapa9@gmail.com>
2024-02-19 20:47:50 +0100

    feat: Add AppFounder

---
+++ src/components/Main/AppFounder.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+    name: 'AppFounder,'
+}
+
+</script>
+
+<template>
+    <section id="founder">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 d-flex justify-content-center align-items-center flex-column">
+                    <h2>Hey there, my name is John Doe. I'm the founder of the Brain Academy</h2>
+                    <img src="../../assets/images/sign.jpg" alt="">
+                </div>
+                <div class="col-6 d-flex justify-content-center align-items-center">
+                    <p>A wonderful serenity has taken possession of my soul, like these sweet mornings of spring which i
+                        enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which wha
+                        created for the bliss of soul of mere tranquil existence, that i neglect my talent. When,while
+                        the lovely valley teems with vapour around me,and the meridian sun strikes the upper surface of
+                        the impenentrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+
+#founder,
+.row {
+    height: 550px;
+}
+
+h2 {
+    margin-top: 150px;
+    padding-left: 120px;
+    padding-right: 120px;
+}
+
+img {
+    margin-top: 40px;
+}
+
+p {
+    font-size: 15px;
+    margin-top: 70px;
+    padding-right: 130px;
+}
+
+
+</style>
--- src/components/Main/AppMain.vue
+++ src/components/Main/AppMain.vue
@@ -1,17 +1,20 @@
 <script>
 import AppJumbo from './AppJumbo.vue';
+import AppFounder from './AppFounder.vue'
 export default {
     components: {
         AppJumbo,
+        AppFounder,
     }
 }
 </script>
 
 <template>
    <main>
-
     <AppJumbo></AppJumbo>
-   
+
+    <AppFounder></AppFounder>   
+    
    </main>
 </template>
 
