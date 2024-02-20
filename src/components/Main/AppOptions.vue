6929a45e14e7c65b408ffe8b0569964ebb202170
PierluigiPapa <pierluigipapa9@gmail.com>
2024-02-20 12:47:18 +0100

    fix: adjust code AppOption with a cicle

--- src/components/Main/AppOptions.vue
+++ src/components/Main/AppOptions.vue
@@ -1,88 +1,59 @@
 <script>
 export default {
     name: 'AppOptions',
+    data() {
+        return {
+            courses: [
+                {
+                    title: 'Entrepreneurship',
+                    description: 'Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate.',
+                    icon: 'fa-solid fa-briefcase'
+                },
+                {
+                    title: 'Accelerated learning',
+                    description: 'Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate.',
+                    icon: 'fa-solid fa-rocket'
+                },
+                {
+                    title: 'Productivity',
+                    description: 'Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate.',
+                    icon: 'fa-solid fa-thumbs-up'
+                },
+                {
+                    title: 'Life Coaching',
+                    description: 'Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate.',
+                    icon: 'fa-solid fa-gear'
+                }
+            ]
+        };
+    }
 }
-
 </script>
 
 <template>
-   <section>
-    <h2 class="text-center py-5">Why my courses?</h2>
-    <div class="container">
-        <div class="row">
-            <div class="col-6 p-0 d-flex justify-content-center">
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4 d-flex justify-content-center mt-5">
-                            <font-awesome-icon icon="fa-solid fa-briefcase" class="fa-solid"/>
-                        </div>
-                        <div class="col-md-8 d-flex justifu-content-center mt-4">
-                            <div class="card-body ps-0">
-                                <h5 class="card-title">Entrepreneurship</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-                                    quibusdam quas corrupti velit consequatur ut recusandae doloremque veritatis, nihil
-                                    mollitia neque incps-0idunt harem.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 p-0 d-flex justify-content-center">
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4 d-flex justify-content-center mt-5">
-                            <font-awesome-icon icon="fa-solid fa-rocket" class="fa-solid"/>
-                        </div>
-                        <div class="col-md-8 d-flex justifu-content-center mt-4">
-                            <div class="card-body ps-0">
-                                <h5 class="card-title">Accelerated learning</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-                                    quibusdam quas corrupti velit consequatur ut recusandae doloremque veritatis, nihil
-                                    mollitia neque incps-0idunt harem.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 p-0 d-flex justify-content-center">
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4 d-flex justify-content-center mt-5">
-                            <font-awesome-icon icon="fa-solid fa-thumbs-up" class="fa-solid"/>
-                        </div>
-                        <div class="col-md-8 d-flex justifu-content-center mt-4">
-                            <div class="card-body ps-0">
-                                <h5 class="card-title">Productivity</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-                                    quibusdam quas corrupti velit consequatur ut recusandae doloremque veritatis, nihil
-                                    mollitia neque incps-0idunt harem.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 p-0 d-flex justify-content-center">
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4 d-flex justify-content-center mt-5">
-                            <font-awesome-icon icon="fa-solid fa-gear" class="fa-solid"/>
-                        </div>
-                        <div class="col-md-8 d-flex justifu-content-center mt-4">
-                            <div class="card-body ps-0">
-                                <h5 class="card-title">Life Coaching</h5>
-                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-                                    quibusdam quas corrupti velit consequatur ut recusandae doloremque veritatis, nihil
-                                    mollitia neque incps-0idunt harem.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-   </section>
-</template>
+    <section>
+     <h2 class="text-center py-5">Why my courses?</h2>
+     <div class="container">
+         <div class="row">
+             <div v-for="(course, index) in courses" :key="index" class="col-6 p-0 d-flex justify-content-center">
+                 <div class="card mb-3" style="max-width: 540px;">
+                     <div class="row g-0">
+                         <div class="col-md-4 d-flex justify-content-center mt-5">
+                             <font-awesome-icon :icon="course.icon" class="fa-solid"/>
+                         </div>
+                         <div class="col-md-8 d-flex justifu-content-center mt-4">
+                             <div class="card-body ps-0">
+                                 <h5 class="card-title">{{ course.title }}</h5>
+                                 <p class="card-text">{{ course.description }}</p>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+    </section>
+ </template>
 
 <style lang="scss" scoped>
 h2 {
@@ -100,5 +71,30 @@ h2 {
     cursor: pointer;
 }
 
+.fa-solid {
+    font-size: 70px;
+    color: #fcb900;
+}
+
+h5 {
+    font-weight: bold;
+    font-size: 25px;
+    color: #333333;
+
+}
+
+.card:hover .fa-solid {
+    color: white;
+}
+
+.card:hover {
+    background-color: #fcb900;
+}
+
+.card-text {
+    font-size: 15px;
+    font-family: sans-serif;
+    color: #333333;
+}
 
 </style>
